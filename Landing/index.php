78cf27e5210a0013b76e156f5ceee848194c1345
CapiMDR <a20201744@alumnos.uady.mx>
2025-11-01 06:23:54 -0600

    Updated UI + changed to dark mode

--- Landing/index.php
+++ Landing/index.php
@@ -14,9 +14,9 @@
     <script src="https://cdn.jsdelivr.net/npm/p5@1.11.8/lib/p5.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/p5@1.11.8/lib/addons/p5.sound.min.js"></script>
     <link rel="stylesheet" type="text/css" href="../Assets/CSS/style.css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <meta charset="utf-8" />
     <script>
-      //Small script to set game mode and submit form automatically
       function setMode(mode) {
         document.getElementById('gameMode').value = mode;
         document.getElementById('gameForm').submit();
@@ -24,21 +24,34 @@
     </script>
   </head>
   <body>
+    <div class="chess-piece">♔</div>
+    <div class="chess-piece">♛</div>
     <div id="container">
       <?php include('navigationBar.php'); ?>
       <div id="menuContainer">
         <div id="leftColumn" class="styledBox">
           <div id="menuBTNS">
             <form id="gameForm" action="/ChessWebsite/Chess/Client/gameIndex.php" method="GET">
-            <input type="hidden" name="mode" id="gameMode" value="">
-            <button class="styledButton btn-large" type="button" onclick="setMode('online')">Play online</button>
-            <button class="styledButton btn-large" type="button" onclick="setMode('bot')">Play vs bot</button>
-            <button class="styledButton btn-large" type="button" onclick="setMode('local')">Play local</button>
-          </form>
+              <input type="hidden" name="mode" id="gameMode" value="">
+              <button class="styledButton btn-large" type="button" onclick="setMode('online')">
+                <span class="material-icons">people</span>
+                Play Online
+              </button>
+              <button class="styledButton btn-large" type="button" onclick="setMode('bot')">
+                <span class="material-icons">smart_toy</span>
+                Play vs Bot
+              </button>
+              <button class="styledButton btn-large" type="button" onclick="setMode('local')">
+                <span class="material-icons">person</span>
+                Play Local
+              </button>
+            </form>
           </div>
         </div>
         <div id="middleContainer">
-            <input type="button" class="btn-styled-red btn-larger" id="playBTN" value="▶" onclick="startGame()"/>
+          <button class="btn-styled-red btn-larger" id="playBTN" onclick="startGame()">
+            <span class="material-icons" style="font-size: 3rem;">play_arrow</span>
+          </button>
         </div>
       </div>
     </div>
