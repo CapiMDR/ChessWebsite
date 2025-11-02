5e168574c4bf51c7826972f139631501f05ff257
CapiMDR <a20201744@alumnos.uady.mx>
2025-11-02 01:49:54 -0600

    Polished UI + added undo/redo move after games

--- Chess/Shared/Move.js
+++ Chess/Shared/Move.js
@@ -6,128 +6,221 @@ Represented as 16 bits (FFFF TTTTTT SSSSSS):
 */
 //Note: a8-a8 = invalid move
 
-import { Piece } from './Piece.js';
-import { BoardUtil } from './BoardUtil.js';
-import {white, black, none, pawn, king,
-  noFlag, enPassantFlag, castleFlag, pawnTwoMoveFlag,
-  promoteKnightFlag, promoteBishopFlag, promoteRookFlag, promoteQueenFlag,} from './Constants.js';
+import { Piece } from "./Piece.js";
+import { BoardUtil } from "./BoardUtil.js";
+import {
+  white,
+  black,
+  none,
+  pawn,
+  king,
+  noFlag,
+  enPassantFlag,
+  castleFlag,
+  pawnTwoMoveFlag,
+  promoteKnightFlag,
+  promoteBishopFlag,
+  promoteRookFlag,
+  promoteQueenFlag,
+} from "./Constants.js";
+import { MoveGenerator } from "./MoveGenerator.js";
 
-export class Move{
-  static newMove(startSqr, targetSqr ,flag=0b0000){
-    return startSqr | targetSqr<<6 | flag<<12;
+export class Move {
+  static newMove(startSqr, targetSqr, flag = 0b0000) {
+    return startSqr | (targetSqr << 6) | (flag << 12);
   }
-  
-  static startSqr(move){
+
+  static startSqr(move) {
     return move & this.startSqrMask;
   }
-  
-  static targetSqr(move){
+
+  static targetSqr(move) {
     return (move & this.targetSqrMask) >> 6;
   }
-  
-  static flag(move){
+
+  static flag(move) {
     return (move & this.flagMask) >> 12;
   }
-  
+
   //Returns what the captured piece would be if the move were played
   //Note: the move must not have been played already
-  static capturePiece(move, board){
-    const enemyClr=(board.clrToMove==white) ? black : white;
-    if(this.flag(move)==enPassantFlag) return Piece.newPiece(pawn,enemyClr);
-    const capturedPiece=board.piecesList[this.targetSqr(move)];
-    return Piece.newPiece(capturedPiece,enemyClr);
+  static capturePiece(move, board) {
+    const enemyClr = board.clrToMove == white ? black : white;
+    if (this.flag(move) == enPassantFlag) return Piece.newPiece(pawn, enemyClr);
+    const capturedPiece = board.piecesList[this.targetSqr(move)];
+    return Piece.newPiece(capturedPiece, enemyClr);
   }
-  
+
   //Returns what the captured piece type would be if the move were played
   //Note: the move must not have been played already
-  static capturePieceType(move, board){
-    if(this.flag(move)==enPassantFlag) return pawn;
-    const capturedPiece=board.piecesList[this.targetSqr(move)];
+  static capturePieceType(move, board) {
+    if (this.flag(move) == enPassantFlag) return pawn;
+    const capturedPiece = board.piecesList[this.targetSqr(move)];
     return Piece.type(capturedPiece);
   }
-  
+
   //Returns if the move is a capture
   //Note: the move must not have been played already
-  static isCapture(move, board){
-    return this.capturePieceType(move,board)!=none;
+  static isCapture(move, board) {
+    return this.capturePieceType(move, board) != none;
   }
-  
+
   //Prints move in 16-bit binary format OR in square (UCI) notation (ex: e2e4)
-  static toString(move, sendBinary=false){
-    if(sendBinary) return move.toString(2).padStart(16, '0');
-    
-    const startSqr=this.startSqr(move);
-    const targetSqr=this.targetSqr(move);
-    let moveString=BoardUtil.sqrToString(startSqr)+BoardUtil.sqrToString(targetSqr)
-    const flag=this.flag(move);
-    
-    switch(flag){
-      case promoteKnightFlag : moveString+='n';
+  static toString(move, sendBinary = false) {
+    if (sendBinary) return move.toString(2).padStart(16, "0");
+
+    const startSqr = this.startSqr(move);
+    const targetSqr = this.targetSqr(move);
+    let moveString = BoardUtil.sqrToString(startSqr) + BoardUtil.sqrToString(targetSqr);
+    const flag = this.flag(move);
+
+    switch (flag) {
+      case promoteKnightFlag:
+        moveString += "n";
         break;
-      case promoteBishopFlag : moveString+='b';
+      case promoteBishopFlag:
+        moveString += "b";
         break;
-      case promoteRookFlag : moveString+='r';
+      case promoteRookFlag:
+        moveString += "r";
         break;
-      case promoteQueenFlag : moveString+='q';
+      case promoteQueenFlag:
+        moveString += "q";
         break;
     }
-    
+
     return moveString;
   }
-  
-  static isPromotion(move){
+
+  static isPromotion(move) {
     const moveFlag = this.flag(move);
-    return (moveFlag==promoteKnightFlag) || (moveFlag==promoteBishopFlag) || (moveFlag==promoteRookFlag) || (moveFlag==promoteQueenFlag);
+    return moveFlag == promoteKnightFlag || moveFlag == promoteBishopFlag || moveFlag == promoteRookFlag || moveFlag == promoteQueenFlag;
   }
-  
+
   //Gets a move in UCI format and transforms it to 16 bits for internal representation (ex e4 = e2e4 -> 0b11100100110100)
   //The board before the move is played is needed to identify its flag correctly
-  static UCIToMove(uci, board){
+  static UCIToMove(uci, board) {
     //First two characters are the starting square
-    const startSquareName=uci.charAt(0) + uci.charAt(1);
+    const startSquareName = uci.charAt(0) + uci.charAt(1);
     //Second two characters are the target square
-    const targetSquareName=uci.charAt(2) + uci.charAt(3);
-    
-    const startSquare=BoardUtil.nameToSquare(startSquareName);
-    const startFile=BoardUtil.squareToFile(startSquare);
-    const startRank=BoardUtil.squareToRank(startSquare);
-    
-    const targetSquare=BoardUtil.nameToSquare(targetSquareName);
-    const targetFile=BoardUtil.squareToFile(targetSquare);
-    const targetRank=BoardUtil.squareToRank(targetSquare);
-    
-    const movedPieceType=Piece.type(board.piecesList[startSquare]);
-    
-    let moveFlag=noFlag;
-    
-    if(movedPieceType==pawn){
-      if(uci.length==5){ //If there is a fifth character it represents a promotion type
-        switch(uci.charAt(4)){
-          case 'n': moveFlag=promoteKnightFlag;
-          break
-          case 'b': moveFlag=promoteBishopFlag;
-          break;
-          case 'r': moveFlag=promoteRookFlag;
-          break;
-          case 'q': moveFlag=promoteQueenFlag;
-          break;
+    const targetSquareName = uci.charAt(2) + uci.charAt(3);
+
+    const startSquare = BoardUtil.nameToSquare(startSquareName);
+    const startFile = BoardUtil.squareToFile(startSquare);
+    const startRank = BoardUtil.squareToRank(startSquare);
+
+    const targetSquare = BoardUtil.nameToSquare(targetSquareName);
+    const targetFile = BoardUtil.squareToFile(targetSquare);
+    const targetRank = BoardUtil.squareToRank(targetSquare);
+
+    const movedPieceType = Piece.type(board.piecesList[startSquare]);
+
+    let moveFlag = noFlag;
+
+    if (movedPieceType == pawn) {
+      if (uci.length == 5) {
+        //If there is a fifth character it represents a promotion type
+        switch (uci.charAt(4)) {
+          case "n":
+            moveFlag = promoteKnightFlag;
+            break;
+          case "b":
+            moveFlag = promoteBishopFlag;
+            break;
+          case "r":
+            moveFlag = promoteRookFlag;
+            break;
+          case "q":
+            moveFlag = promoteQueenFlag;
+            break;
         }
-      }else if(Math.abs(targetRank-startRank)==2){ //If the pawn is moving 2 ranks it is a double push
-        moveFlag=pawnTwoMoveFlag;
-      }else if(startFile!=targetFile && Piece.type(board.piecesList[targetSquare])==none){
+      } else if (Math.abs(targetRank - startRank) == 2) {
+        //If the pawn is moving 2 ranks it is a double push
+        moveFlag = pawnTwoMoveFlag;
+      } else if (startFile != targetFile && Piece.type(board.piecesList[targetSquare]) == none) {
         //If the pawn is changing files but there is no piece in its target square it's en passant
-        moveFlag=enPassantFlag;
+        moveFlag = enPassantFlag;
       }
     }
-    
+
     //If the king is moving 2 squares it must be castling
-    if(movedPieceType==king && Math.abs(targetFile-startFile)==2){
-      moveFlag=castleFlag;
+    if (movedPieceType == king && Math.abs(targetFile - startFile) == 2) {
+      moveFlag = castleFlag;
     }
-    return this.newMove(startSquare,targetSquare,moveFlag);
+    return this.newMove(startSquare, targetSquare, moveFlag);
+  }
+
+  //Given a UCI formatted move, return the SAN (Standard algebraic notation) format (ex: g1f3 could be equal to Nf3)
+  static UCIToSAN(uci, board) {
+    const from = BoardUtil.nameToSquare(uci.slice(0, 2));
+    const to = BoardUtil.nameToSquare(uci.slice(2, 4));
+    const promotion = uci.length === 5 ? uci[4] : null;
+
+    const movingPiece = board.piecesList[from];
+    const pieceType = Piece.type(movingPiece);
+    const moveGen = new MoveGenerator();
+
+    //Handle castling
+    if (pieceType === king && Math.abs(BoardUtil.squareToFile(to) - BoardUtil.squareToFile(from)) === 2) {
+      return BoardUtil.squareToFile(to) > BoardUtil.squareToFile(from) ? "O-O" : "O-O-O";
+    }
+
+    //Determine base letter (omit for pawns)
+    const pieceLetter = ["", "", "N", "B", "R", "Q", "K"][pieceType];
+
+    //Detect if it's a capture
+    const isCapture = this.isCapture(this.UCIToMove(uci, board), board);
+
+    let disambiguation = "";
+    //Determine disambiguation if needed (same piece type can move to the same square)
+    if (pieceType !== pawn && pieceType !== king) {
+      const moves = moveGen.generateMoves(board);
+      const samePieceMoves = moves.filter((mv) => {
+        return Move.targetSqr(mv) === to && Move.startSqr(mv) !== from && Piece.type(board.piecesList[Move.startSqr(mv)]) === pieceType;
+      });
+      if (samePieceMoves.length > 0) {
+        const fromFile = BoardUtil.squareToFile(from);
+        const fromRank = BoardUtil.squareToRank(from);
+        const fileConflict = samePieceMoves.some((mv) => BoardUtil.squareToFile(Move.startSqr(mv)) === fromFile);
+        const rankConflict = samePieceMoves.some((mv) => BoardUtil.squareToRank(Move.startSqr(mv)) === fromRank);
+
+        if (!fileConflict) disambiguation = BoardUtil.fileToChar(fromFile);
+        else if (!rankConflict) disambiguation = (fromRank + 1).toString();
+        else disambiguation = BoardUtil.fileToChar(fromFile) + (fromRank + 1);
+      }
+    }
+
+    //Pawn move captures must include the origin file (e.g. exd5)
+    let san = "";
+    if (pieceType === pawn) {
+      if (isCapture) san += BoardUtil.fileToChar(BoardUtil.squareToFile(from));
+    } else {
+      san += pieceLetter + disambiguation;
+    }
+
+    if (isCapture) san += "x";
+
+    san += BoardUtil.indexToName(to);
+
+    if (promotion) {
+      san += "=" + promotion.toUpperCase();
+    }
+
+    const moveToMake = Move.UCIToMove(uci, board);
+    //Make the move temporarily to check for check or checkmate
+    board.makeMove(moveToMake);
+    const legalMoves = moveGen.generateMoves(board);
+    const inCheck = moveGen.inCheck;
+    const isMate = inCheck && legalMoves.length === 0;
+    board.unmakeMove(moveToMake);
+
+    if (isMate) san += "#";
+    else if (inCheck) san += "+";
+
+    return san;
   }
 }
 
-Move.startSqrMask=0b0000000000111111;
-Move.targetSqrMask=0b0000111111000000;
-Move.flagMask=0b1111000000000000;
+Move.startSqrMask = 0b0000000000111111;
+Move.targetSqrMask = 0b0000111111000000;
+Move.flagMask = 0b1111000000000000;
